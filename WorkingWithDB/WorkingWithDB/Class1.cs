3c059a1b2d74609af8711f4a1c918711bba95532
CrazyStilet <Stilet-2006@yandex.ru>
2016-05-12 09:39:22 +0300

    Класс для работы с БД

---
+++ WorkingWithDB/WorkingWithDB/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDB
+{
+    public class WorkingWithDB
+    {
+        public static string Show()
+        {
+            return "WorkingWithDB";
+        }
+    }
+}
